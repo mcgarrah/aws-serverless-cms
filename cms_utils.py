99ef149c98cbbb3b78e25a8f277e286a783e5bb3
Gabriel Wu <yingpingwdz@163.com>
2017-02-24 15:26:28 +0800

    comment most functions change the way user interact

---
+++ cms_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+# cms_functions.py
+# Author: Gabriel Wu
+# Date: N/D
+# Edited: 02/24/2017 | Gabriel Wu
+"""
+
+import boto3
+import botocore
+
+
+class CmsUtils:
+
+    def __init__(self):
+        print self
+
+    @staticmethod
+    def getAvailableRegions():
+        client = boto3.client('ec2')
+        regions = [region['RegionName'] for region in
+                   client.describe_regions()['Regions']]
+        return regions
+
+
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 """
 # setup.py
@@ -10,58 +11,63 @@
 import cms_functions
 import sys
 import os
+import cms_utils
 
-if len(sys.argv) not in range(2, 4):
-    command = ''
-    for arg in sys.argv:
-        command += arg + ' '
-    print 'Invalid command: ' + command
-    print 'Usage: %s <cms_prefix> <region (optional)>' % sys.argv[0]
-    sys.exit()
+regions = cms_utils.CmsUtils.getAvailableRegions()
 
-# Instantiate an AwsFunc class
-if len(sys.argv) == 3:
-    cms = cms_functions.AwsFunc(sys.argv[1], region=sys.argv[2])
-else:
-    cms = cms_functions.AwsFunc(sys.argv[1])
+for region in regions:
+    print region
+
+selectedRegion = raw_input('select a region to deploy cms: ')
+cms_name = raw_input('input name of your cms: ')
+
+if selectedRegion not in regions:
+    print selectedRegion, \
+        ' is not available, choose default region us-east-1'
+    selectedRegion = 'us-east-1'
+
+print selectedRegion, ' is selected and cms with name = ', cms_name
+
+cms = cms_functions.AwsFunc(cms_name, region=selectedRegion)
 
 # Create tje rest api
-cms.create_rest_api()
 
-# Create the lambda function
-cms.create_lambda_function()
+# cms.create_rest_api()
 
-# Setup the rest api
-cms.api_add_post_method()
-cms.api_add_options_method()
-cms.deploy_api()
-
-# Create the s3 bucket
-cms.create_bucket()
-# Create the cloudfront distribution
-# cms.create_cloudfront_distribution() TODO: Reactivate
-
-# Create the dynamodb blog table
-cms.create_blog_table()
-
-# Create the dynamodb page table
-cms.create_page_table()
-
-# Create the dynamodb token table
-cms.create_token_table()
-
-# Create the dunamodb role table
-cms.create_role_table()
-# Add an admin role to the role table
-cms.create_admin_role_db_entry()
-
-# Create the dynamodb user table
-cms.create_user_table()
-# Add an admin to the user table
-cms.create_admin_user_db_entry()
-
-# Print the default login credentials and the login link
-cms.print_login_link()
-
-# Saves the cms installation information
-cms.save_constants()
+## Create the lambda function
+# cms.create_lambda_function()
+#
+## Setup the rest api
+# cms.api_add_post_method()
+# cms.api_add_options_method()
+# cms.deploy_api()
+#
+## Create the s3 bucket
+# cms.create_bucket()
+## Create the cloudfront distribution
+## cms.create_cloudfront_distribution() TODO: Reactivate
+#
+## Create the dynamodb blog table
+# cms.create_blog_table()
+#
+## Create the dynamodb page table
+# cms.create_page_table()
+#
+## Create the dynamodb token table
+# cms.create_token_table()
+#
+## Create the dunamodb role table
+# cms.create_role_table()
+## Add an admin role to the role table
+# cms.create_admin_role_db_entry()
+#
+## Create the dynamodb user table
+# cms.create_user_table()
+## Add an admin to the user table
+# cms.create_admin_user_db_entry()
+#
+## Print the default login credentials and the login link
+# cms.print_login_link()
+#
+## Saves the cms installation information
+# cms.save_constants()
